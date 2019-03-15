3f1476aeb1fed3abe050ccc8ed7fb88f74be4eb1
Dmitrij Lucenko <333333y@gmail.com>
2019-03-15 15:13:40 +0545

    uikit toggler addon

--- src/scripts/vendors/uikit.js
+++ src/scripts/vendors/uikit.js
@@ -3,3 +3,44 @@ import Icons from 'uikit/dist/js/uikit-icons';
 
 // loads the Icon plugin
 UIkit.use(Icons);
+
+$(() => {
+    //toggler aadon
+    (function () {
+        const $togglers = $('[uk-toggle]');
+
+        $togglers.each(function () {
+            const $toggler = $(this);
+            const $target = $(getTarget($toggler));
+            const activeClassName = 'uk-active';
+
+            $target.on({
+                'shown': function () {
+                    $toggler.addClass(activeClassName);
+                },
+                'hidden': function () {
+                    $toggler.removeClass(activeClassName);
+                }
+            });
+
+        });
+
+        function getTarget($toggler) {
+            const patern = 'target:';
+            const str = $toggler.attr('uk-toggle') || $toggler.attr('href');
+
+            if (!str) return null;
+
+            if (str.indexOf(patern) === -1) {
+                return str;
+            }
+
+            const reg = /target:(.+)\;/i;
+            const match = str.match(reg);
+
+            if (!match || !match[1]) return null;
+
+            return match[1];
+        }
+    })();
+});
